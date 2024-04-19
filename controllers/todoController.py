74c2c04009eeddbe67825cb7059b90b2090830cb
trandung2k1 <trandungitksnb2001@gmail.com>
2024-04-19 22:46:16 +0700

    update todoController

--- controllers/todoController.py
+++ controllers/todoController.py
@@ -1,4 +1,18 @@
+from flask import jsonify
+from configs.db import MyDB
+mycursor = MyDB.cursor()
+
 class TodoController:
     @staticmethod
     def index():
-        return {"status": "OK"}, 200
+        mycursor.execute("SELECT * FROM todos")
+        myresult = mycursor.fetchall()
+        rs = []
+        for x in myresult:
+            rs.append(
+                {'id': x[0], 'title': x[1], 'des': x[2], 'completed': "false" if x[3] == 0 else 'true'})
+        return jsonify({
+            "status": 200,
+            "data": rs
+        })
+        
